f003aae881485458616a80320ee5efc54d03d4e5
vacuousplanet <kagelosaurus@gmail.com>
2021-02-01 00:40:27 -0800

    reformat some socket comms

--- src/index.ts
+++ src/index.ts
@@ -21,15 +21,28 @@ interface userSocket extends socketIO.Socket {
     lobby_code: string | undefined;
 }
 
+// initialization object from client
+interface LobbySettings {
+    username: string;
+    gamepath: string;
+    lobby_size: number;
+    lobby_code: string;
+    lobby_password: string;
+}
+
 // lobby and user validation middleware
+/*
 io.use((socket: userSocket, next) => {
     // check for lobby existance
+    console.log(`middleware hit: ${socket.}`)
     if (socket.lobby_code !== undefined && !lobbies.has(socket.lobby_code)) {
+        console.log('no lobby there')
         next(new Error(`No lobby with code ${socket.lobby_code} could be found`));
         return;
     }
     // check for user in lobby
     if (lobbies.get(socket.lobby_code).getPlayerState(socket.username) === undefined) {
+        console.log('user not in lobby')
         next(new Error(`Not authorized to access ${socket.lobby_code}`));
         return;
     }
@@ -37,13 +50,16 @@ io.use((socket: userSocket, next) => {
     // continue routing otherwise
     next();
 });
-
+*/
 
 io.on('connection', (socket: userSocket) => {
 
-    // create lobby signal
-    socket.on('create', (username: string, game_version: string, lobby_size: number) => {
+    console.log('yo waddup')
 
+    // create lobby signal
+    socket.on('create', (lobby_settings: LobbySettings, callback: (a: LobbySettings, status: string) => void) => {
+        // TODO: decode lobby_settings and use callback
+        console.log(lobby_settings);
         // generate random 6 digit code and verify that it's new
         var new_code = Math.random().toString(36).substr(2, 6);
         while(new_code in lobbies){
@@ -53,13 +69,17 @@ io.on('connection', (socket: userSocket) => {
         // create lobby @ new code
         lobbies.set(
             new_code,
-            new Lobby('lmao', game_version, lobby_size, new_code)
+            new Lobby('lmao', lobby_settings.gamepath, lobby_settings.lobby_size, new_code)
         );
 
+        lobby_settings.lobby_code = new_code;
+        lobby_settings.lobby_password = 'lmao';
+
         // try adding player to lobby
-        const revised_username = lobbies.get(new_code).addPlayer(username, 'lmao');
+        const revised_username = lobbies.get(new_code).addPlayer(lobby_settings.username, 'lmao');
         if (revised_username === undefined) {
             // lobby full (idk how to handle wrong pw's yet)
+            callback(lobby_settings, 'bad')
             return;
         }
 
@@ -70,23 +90,37 @@ io.on('connection', (socket: userSocket) => {
         socket.join(new_code);
 
         // send username back to client
-        socket.emit('lobbyJoined', new_code, revised_username);
+        callback(lobby_settings, 'ok');
     });
 
     // join lobby signal
-    socket.on('join', (username: string, lobby_code: string, password: string) => {
-        const revised_username = lobbies.get(lobby_code).addPlayer(username, 'lmao');
+    socket.on('join', (lobby_settings: LobbySettings, callback: (a: LobbySettings, status: string) => void) => {
+
+        const revised_username = lobbies.get(lobby_settings.lobby_code).addPlayer(lobby_settings.username, 'lmao');
         if (revised_username === undefined) {
             // lobby full (idk how to handle wrong pw's yet
+            callback(lobby_settings, 'bad');
             return;
         }
 
         socket.username = revised_username;
-        socket.lobby_code = lobby_code;
-        socket.join(lobby_code);
+        socket.lobby_code = lobby_settings.lobby_code;
+        socket.join(lobby_settings.lobby_code);
 
-        socket.emit('lobbyJoined', lobby_code, revised_username);
+        callback(lobby_settings, 'ok');
+
+        socket.to(socket.lobby_code).emit('player-join', `${socket.username} has joined the lobby...`);
+
+        //socket.emit('lobbyJoined', lobby_settings.lobby_code, revised_username);
     });
+
+
+    // ready player signal
+    socket.on('ready', () => {
+        console.log(`hey there ${socket.username} in: ${socket.lobby_code}`);
+        console.log(socket.rooms);
+        socket.to(socket.lobby_code).emit('player-ready', `${socket.username} is ready...`)
+    })
 
     // state update signal
     socket.on('beat-gym', () => {
@@ -149,6 +183,6 @@ app.post('/lobby/:lobbyID/logout', (req, res) => {
 });
 */
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
